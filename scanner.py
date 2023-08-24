445a625333b77230a423a982abf5af7d07add4e4
patrickdx <blah0833@gmail.com>
2023-08-24 11:35:01 -0400

    Add files via upload

---
+++ scanner.py
@@ -0,0 +1,41 @@
+import cv2 as cv
+import numpy as np 
+
+# img = cv.imread('./images/power-BI.jpg')   # default loaded in BGR format
+# cv.imshow("window", img)
+# print(type(img))
+# if (cv.waitKey(1000) == ord('q')): print("XD")       # wait for x ms for user input before continuing, 0 is infinite
+
+# print(img)
+
+# video = cv.VideoCapture('./vods/1879662076.mp4')   # VideoCapture object to load in video
+
+# while video.isOpened(): 
+#     bool, frame = video.read()      # reads a frame (Matrix); returns true if successful
+#     if (bool == False):             # this line is nessacary for exiting the loop     
+#         print("could not read frame. video end?...")
+#         break
+
+#     print(video.get(cv.CAP_PROP_FRAME_HEIGHT))      # cap.get()/set() for properties of the video
+
+#     cv.rectangle(frame, (0,0), (500,500), (255,0,0) ,3)
+
+#     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)        # convert frame to grayscale
+#     cv.imshow('window', gray)
+#     print(gray)
+#     if cv.waitKey(100000000) == ord('q'): break             # waitKey returns unicode value, compare with q for quit
+#                                                     # lower value = speed up video, vice versa   
+
+
+# video.release()
+# cv.destroyAllWindows()
+
+
+#--------------------------------------------------------------
+
+
+img = np.zeros((300,512,3), np.uint8)               # 300 x 512 window with 3 channels
+print(len(img[0]))
+cv.imshow('window', img)
+print(img.shape)
+cv.waitKey(0)
---
+++ spreadsheet.py
@@ -0,0 +1,69 @@
+
+import pandas as pd
+import sys
+import time
+''' 
+A spreadsheet containing raw run data only, can fill with aggregations later (avg times of all cols, etc...)
+'''
+# https://stackoverflow.com/questions/13331518/how-to-add-a-single-item-to-a-pandas-series
+
+def period():        # returns a generator object        
+    animation = ['.  ','.. ','...']
+    count = 0
+    while True:
+        yield animation[count]
+        count += 1
+        count = count % 3 
+
+
+def progress(current, total, animation = period(), status=''):      # question is what to base progress of? percentage of vod? percentage of run?
+    current, total = int(current), int(total)
+    bar_len = 60                        # 60 chars long
+    filled = current / total            # actual progress percentage
+    filledBars = '=' * round(bar_len * filled)  # 9.6 -> 10 bars 
+    rest = '-' * (bar_len - len(filledBars))
+    sys.stdout.write("[%s] %s %s/%s %s\r" % (filledBars + rest, status, current, total, next(animation)))  
+
+# for i in range(1,101):
+#     progress(i,100, status = 'generating god seed')
+#     time.sleep(0.5)
+
+
+cols = [    
+    'video_url',        
+    'date',
+    'timestamp',
+    'reset_time', 
+    'nether_entry',
+    'found_bastion',
+    'found_fortress',
+    'nether_exit',
+    'stronghold_entry',
+    'end_entry',
+    'dragon_kill',
+    'elasped_time',
+]
+
+
+df = pd.DataFrame(columns=cols)
+sample_row = ['Videos/12321321', '07/18/23', '03:23:05', '15:38', '05:01', '07:48', '11:43'	,'13:01', '15:54', '16:07',	'18:03'	,'18:12']
+df.loc[0] = sample_row      # add a new row starting at index 0
+
+
+# convert time cols to pandas datetime obj type
+
+df['date'] = pd.to_datetime(df['date'])   
+df['timestamp'] = pd.to_datetime(df['timestamp'], format = '%H:%M:%S')
+
+# for cols that track timer 
+for col in df.columns.values[3:]:
+    # print(df.columns.values)
+    df[col] = pd.to_datetime(df[col], format = '%M:%S')
+
+# print(df.dtypes)
+
+def recordValue(col, time):
+    df[col] = pd.concat([df[col], pd.Series([time])])      #https://stackoverflow.com/questions/13331518/how-to-add-a-single-item-to-a-pandas-series
+
+def recordRow(row : list[tuple]):       # TODO: (col, value) add data to df as incomplete row....
+    pass
